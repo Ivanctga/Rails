5196b097d0814a6b4fbd6041329bb512717d52d8
Ivanctga <ivanctga@gmail.com>
2025-03-06 21:14:38 -0300

    1° commit - 06/03

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,10 @@
 jobs = {
   'new_post_notification_job' => {
-    'cron' => '* * * * *',
+    'cron' => '* 10 * * 6',
     'class' => 'NewPostsNotificationJob',
     'queue' => 'notification',
     'active_job' => true
   }
 }
 
-Sidekiq::Cron::Job.load_from_hash!(jobs)
+Sidekiq::Cron::Job.load_from_hash(jobs)
